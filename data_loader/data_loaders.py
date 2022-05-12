b48b1deff9375e0f62e981422808141712e46067
Pieter Tolsma <epieter.tolsma@gmail.com>
2022-05-12 16:51:35 +0200

    dataloading

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,51 @@
+
 from torchvision import datasets, transforms
+from torchvision.io import read_image
 from base import BaseDataLoader
+from torch.utils.data import Dataset
 
+import os, os.path
 
-class MnistDataLoader(BaseDataLoader):
+class TomatoImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None, mask_transform=None):
+        self.img_dir = root_dir + "/images"
+        self.img_files = os.listdir(self.img_dir)
+        self.mask_dir = root_dir + "/masks"
+        self.mask_files = os.listdir(self.mask_dir)
+        self.transform = transform
+        self.mask_transform = mask_transform
+
+    def __len__(self):
+        return len(self.mask_files)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_files[idx])
+        mask_path = os.path.join(self.mask_dir, self.mask_files[idx])
+
+        image = read_image(img_path)
+        mask = read_image(mask_path)
+        if self.transform:
+            image = self.transform(image)
+        
+        if self.mask_transform:
+            mask = self.mask_transform(mask)
+
+        return image, mask
+
+class TomatoDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+            transforms.Resize(size=(256, 256)),
+            transforms.Grayscale()
+        ])
+        m_trsfm = transforms.Compose([
+            transforms.Resize(size=(256, 256)),
+            transforms.FloatToByteTensor()
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = TomatoImageDataset(self.data_dir, transform=trsfm, mask_transform=m_trsfm)
+        print("DataLoader:", len(self.dataset))
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
