4f9089970fbdedd7429aebe2dccff28afeebbce0
Pieter Tolsma <epieter.tolsma@gmail.com>
2022-05-18 13:49:02 +0200

    Latest version

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,7 @@
 
 from math import floor
+import torch
+import random
 from torchvision import datasets, transforms
 import torchvision.transforms.functional as F
 from torchvision.io import read_image
@@ -82,6 +84,11 @@ class TomatoImageDataset(Dataset):
         image = read_image(img_path)
         mask = read_image(mask_path)
 
+        # Ensure that both random operations are the same
+        seed = np.random.randint(2147483647)
+        torch.manual_seed(seed)
+        random.seed(seed)
+
         if self.transform:
             image = self.transform(image)
         
@@ -95,31 +102,19 @@ class TomatoDataLoader(BaseDataLoader):
     MNIST data loading demo using BaseDataLoader
     """
 
-    # def preprocess(pil_img, scale, is_mask):
-    #     w, h = pil_img.size
-    #     newW, newH = int(scale * w), int(scale * h)
-    #     assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
-    #     pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
-    #     img_ndarray = np.asarray(pil_img)
-
-    #     if not is_mask:
-    #         if img_ndarray.ndim == 2:
-    #             img_ndarray = img_ndarray[np.newaxis, ...]
-    #         else:
-    #             img_ndarray = img_ndarray.transpose((2, 0, 1))
-
-    #         img_ndarray = img_ndarray / 255
-
-    #     return img_ndarray
-
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             FilterVines(),
-            PadImage()
+            PadImage(),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(90),
         ])
         m_trsfm = transforms.Compose([
-            PadImage()
+            PadImage(),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(90),
         ])
+
         self.data_dir = data_dir
         self.dataset = TomatoImageDataset(self.data_dir, transform=trsfm, mask_transform=m_trsfm)
 
