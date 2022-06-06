8e3866c9cc83ddd8ea6ac2ea52942f35803abb12
cornelis <cornelis.zwart.cz@gmail.com>
2022-06-06 15:40:34 +0200

    it can transform and inverse transform correctly it also calculates the intersection of the transformed images prediction
    however for some reason it is negative and orders of magnitude wrong
    this is a error somewhere in the labereler.py most likely, since running it on an image from the test folder yields a different output than when using test.py

--- labeler.py
+++ labeler.py
@@ -23,9 +23,10 @@ def custom_loss(pred, target):
     tflat = target.view(-1)
     return 1 - (iflat - iflat * tflat).sum() / (iflat.sum() + 1e-6)
 
+
 def dataloader(img_dir):
     print(img_dir)
-    #read image
+    # read image
 
     files = os.listdir(img_dir)
     file = files[0]
@@ -41,33 +42,34 @@ def dataloader(img_dir):
 
     img = transform(img)
     imgs = [img]
-    #rotate 90,180 and 270 deg
-    for i in range(3):
-        imma = F.rotate(imgs[0],90*i)
+    # rotate 90,180 and 270 deg
+    for i in range(1,4):
+        imma = F.rotate(imgs[0], 90 * i)
         imgs.append(imma)
 
-    assert(len(imgs) == 4)
+    assert (len(imgs) == 4)
     out = []
-    #for each of those
+    # for each of those
     for img in imgs:
         out.append(img)
-        #flip vertically
+        # flip vertically
         out.append(transforms.functional.hflip(img))
-        #flip horizontally
+        # flip horizontally
         out.append(transforms.functional.vflip(img))
-
+    # visualizeRotations(out)
     return out
 
 def inverseAugmentation(datas):
     out = []
-    for i in range(len(datas),step=3):
-        out.append(F.rotate(datas[i], -i*30))#looks strange but at i=0 give 0deg, i=3 give 90deg etc
-        out.append(F.rotate(F.hflip(datas[i]),-i*30))
-        out.append(F.rotate(F.vflip(datas[i]),-i*30))
+    for i in range(0, len(datas), 3):
+        out.append(F.rotate(datas[i], -i * 30).squeeze())  # looks strange but at i=0 give 0deg, i=3 give 90deg etc
+        out.append(F.rotate(F.hflip(datas[i+1]), -i * 30).squeeze())
+        out.append(F.rotate(F.vflip(datas[i+2]), -i * 30).squeeze())
 
     return out
 
-#TODO: Doesn't work for reasons unkown to mankind
+
+# TODO: Doesn't work for reasons unkown to mankind
 def visualizeRotations(imgs):
     cols = 6
     rows = 2
@@ -75,7 +77,7 @@ def visualizeRotations(imgs):
 
     for a in range(rows):
         for b in range(cols):
-            axes[a, b].imshow(imgs[b * cols + a][0,:,:], cmap=cm.jet)
+            axes[a, b].imshow(imgs[b * cols + a][0, :, :], cmap=cm.jet)
 
     plt.tight_layout()
     plt.show()
@@ -87,7 +89,8 @@ def main(config):
     logger = config.get_logger('test')
 
     # setup data_loader instances
-    file = os.path.join(config['data_loader']['args']['data_dir'], os.listdir(config['data_loader']['args']['data_dir'])[0])
+    file = os.path.join(config['data_loader']['args']['data_dir'],
+                        os.listdir(config['data_loader']['args']['data_dir'])[0])
     # data_loader = TomatoPredDataLoader(path=file,batch_size=2,shuffle=False)
 
     # build model architecture
@@ -118,7 +121,8 @@ def main(config):
     outs = []
     with torch.no_grad():
         for i, data in enumerate(tqdm(datas)):
-            data, target = data.to(device)
+            data = data.reshape(1, data.shape[0], data.shape[1], data.shape[2], )
+            data = data.to(device)
             output = model(data)
             a = output
             d = data
@@ -131,16 +135,62 @@ def main(config):
 
     ogs = inverseAugmentation(outs)
     result = np.zeros((ogs[0].shape))
-    for i in range(ogs):
+    for i in range(len(ogs)):
+        assert (ogs[0].shape == ogs[i].shape)
 
+    displayPredictions(ogs)
 
+    for o in ogs:
+        for i in range(result.shape[0]):
+            for j in range(result.shape[1]):
+                result[i, j] += o[i, j] > config["cutoff"]
 
+    result *= 32
+    print(result.shape)
+    print(ogs[0].shape)
+
+    fig, axes = plt.subplots(1, 4, figsize=(15, 5), sharex=True, sharey=True)
+    ax = axes.ravel()
+
+    ax[0].imshow(result, cmap=cm.jet)
+    ax[0].set_title('Intersection')
+
+    ax[1].imshow(ogs[0])
+    ax[1].set_title('Target mask')
+
+    ax[2].imshow(ogs[3])
+    ax[2].set_title('Pred mask')
+
+    original_input = datas[0]
+    original_input = original_input.transpose(0, 2)
+    original_input = original_input.transpose_(0, 1)
+    ax[3].imshow(original_input)
+    ax[3].set_title('Original')
+
+    plt.tight_layout()
+    plt.show()
+
+    # print(result)
     n_samples = len(datas)
     log = {'loss': total_loss / n_samples}
     log.update({
         met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
     })
     logger.info(log)
+
+def displayPredictions(datas):
+    cols = 4
+    rows = 3
+    fig, axes = plt.subplots(rows, cols, figsize=(15, 15), sharex=True, sharey=True)
+
+    for a in range(rows):
+        for b in range(cols):
+            if a * cols + b < len(datas):
+                axes[a, b].imshow(datas[a * cols + b], cmap=cm.jet)
+
+    plt.tight_layout()
+    plt.show()
+    plt.close()
 
 
 if __name__ == '__main__':
