b2ccefc467c28d1b9c1173a890425101a9a85eb6
Pieter Tolsma <epieter.tolsma@gmail.com>
2022-06-07 11:40:17 +0200

    Also save overlay mask

--- utils/data_saver.py
+++ utils/data_saver.py
@@ -6,6 +6,13 @@ import numpy as np
 import cv2
 import torch
 
+import math
+
+def generate_mask_overlay(original, mask):
+    new = original
+    new[:, :, 1] = np.clip(original[:, :, 1] + 0.5 * mask[:, :], 0, 1)
+    return new
+
 def save_image(folder, original_image, mask, filename):
     """
     Saves the original image and the mask to a folder.
@@ -15,6 +22,7 @@ def save_image(folder, original_image, mask, filename):
         os.makedirs(folder + "/images")
         os.makedirs(folder + "/masks")
         os.makedirs(folder + "/visible_masks")
+        os.makedirs(folder + "/overlay_masks")
 
     # files = os.listdir(f'{folder}/images')
     # file_count = len(files)
@@ -22,13 +30,18 @@ def save_image(folder, original_image, mask, filename):
     image_name = f'images/{filename}.png'
     mask_name = f'masks/mask_{filename}.png'
     visible_mask_name = f'visible_masks/mask_{filename}.png'
+    overlay_mask_name = f'overlay_masks/mask_{filename}.png'
     original_image_path = os.path.join(folder, image_name)
     mask_path = os.path.join(folder, mask_name)
     visible_mask_path = os.path.join(folder, visible_mask_name)
+    overlay_mask_path = os.path.join(folder, overlay_mask_name)
 
     plt.imsave(original_image_path, original_image)
     cv2.imwrite(mask_path, mask)
     plt.imsave(visible_mask_path, mask)
+
+    mask_overlay = generate_mask_overlay(original_image, mask)
+    plt.imsave(overlay_mask_path, mask_overlay)
 
 
 # test_image = plt.imread('../data/grabpoints/test/images/image_0_0.png')
--- validate_masks.py
+++ validate_masks.py
@@ -4,6 +4,11 @@ import cv2
 import numpy as np
 import math
 
+def generate_mask_overlay(original, mask):
+    new = original
+    new[:, :, 1] = original[:, :, 1] + 0.5 * mask[:, :, 0]
+    return new
+
 folder = "new_output"
 
 original_images = os.listdir(f'{folder}/images')
@@ -22,15 +27,11 @@ for img_file in original_images:
     mask_file = masks[i]
 
     original_img = plt.imread(f'{folder}/images/{img_file}')
-
     mask_img = cv2.imread(f'{folder}/masks/{mask_file}')
 
-    original_img[:, :, 1] = original_img[:, :, 1] + 0.5 * mask_img[:, :, 0]
-
-    masked = np.ma.masked_where(mask_img == 0, original_img[:, :, 0:3])
+    original_img = generate_mask_overlay(original_img, mask_img)
 
     fig.add_subplot(width, width, i + 1)
-
     plt.imshow(original_img, interpolation="none")
 
     i += 1
